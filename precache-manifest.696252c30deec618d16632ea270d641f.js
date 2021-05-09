6592581c097a8b06491cea72f3e0d0cc8ace16ad
bde_100008627096_erica <bde_100008627096_erica@git.cloud.tencent.com>
2021-05-09 20:54:31 +0800

    deploy

---
+++ precache-manifest.696252c30deec618d16632ea270d641f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60ae893259a84858b646",
+    "url": "/jrg-Vue-Wangcai-14-03/css/app.dd762e8d.css"
+  },
+  {
+    "revision": "e69889f7eaf3dc0a795a030e6ccf95cc",
+    "url": "/jrg-Vue-Wangcai-14-03/index.html"
+  },
+  {
+    "revision": "60ae893259a84858b646",
+    "url": "/jrg-Vue-Wangcai-14-03/js/app.c5ba1e35.js"
+  },
+  {
+    "revision": "0ebfcb7b8e8a7fdc8c5b",
+    "url": "/jrg-Vue-Wangcai-14-03/js/chunk-vendors.c53a9c6c.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/jrg-Vue-Wangcai-14-03/manifest.json"
+  },
+  {
+    "revision": "d3829550cf8facc6c7dc650bdaf6761d",
+    "url": "/jrg-Vue-Wangcai-14-03/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/jrg-Vue-Wangcai-14-03/robots.txt"
+  }
+]);
